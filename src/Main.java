833f8f71b20091c472e92a9f06c0e6665df69a5c
ubaeida <ubaeida.alkayal@gmail.com>
2026-05-21 23:54:20 +0300

    Java OOP

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student(1,"Ubaeida", "ALkayal");
+        System.out.println(s1.getId());
+        System.out.println(s1.getFirstName());
+        System.out.println(s1.getLastName());
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,41 @@
+
+// creating new class
+public class Student {
+    //Properties
+    private int  id;
+    private String firstName;
+    private String lastName;
+
+    //Constructor to initialize object state
+    public Student(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    //Getter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    //Setter
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
